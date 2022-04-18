1307795059d2e8550bb86fa644449baa5ec8ca2f
Kranthi babu <91652767+KRAN22@users.noreply.github.com>
2022-04-18 19:26:06 +0530

    Create class_file.py

---
+++ Decarators/argrsPackages/class_file.py
@@ -0,0 +1,10 @@
+class StartStopwitharguement:
+    def __init__(self,name4,name5,name6):
+        self.name4 = name4
+        self.name5 = name5
+        self.name6 = name6
+        def __init__(self,function):
+            self.function = function
+            
+        def __call__(self,*args):
+            self.function(*args)
