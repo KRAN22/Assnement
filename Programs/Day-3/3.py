8ea527994c5fe396bf0d4cff5c83a0be42db0cb0
Kranthi babu <91652767+KRAN22@users.noreply.github.com>
2022-04-19 08:39:50 +0530

    Create 3.py

---
+++ Programs/Day-3/3.py
@@ -0,0 +1,7 @@
+# 3. Write a Python program to iterate over dictionaries using for loops
+
+def iterate(dict):
+    for key in dict:
+        print( key , dict[key])
+
+iterate({'a': 'juice', 'b': 'grill', 'c': 'corn'})
